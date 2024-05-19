029ab0889d1b821183f00aa84ab74d0bc84693be
Exitare <business.exitare@icloud.com>
2024-05-18 21:27:35 -0700

    Add number of available sentences per category.

--- src/app/explore/explore.page.ts
+++ src/app/explore/explore.page.ts
@@ -3,6 +3,7 @@ import {SentencesService} from "../../core/services/sentence.service";
 import {Subscription} from "rxjs";
 import {SettingsService} from "../../core/services/settings.service";
 import {NavController} from "@ionic/angular";
+import {TranslateService} from "@ngx-translate/core";
 
 
 @Component({
@@ -14,15 +15,32 @@ export class ExplorePage implements OnInit, OnDestroy {
     subscriptions$: Subscription[] = [];
     availableCategories: string[] = [];
     colors: string[] = []
+    sentenceCountByCategory: any = {};
 
-    constructor(private sentenceService: SentencesService, private settingsService: SettingsService, private navCtrl: NavController) {
-        this.subscriptions$.push(this.sentenceService.loadAvailableCategories().subscribe((categories) => this.availableCategories = categories));
+    constructor(private sentenceService: SentencesService, private settingsService: SettingsService, private navCtrl: NavController
+        , private translateService: TranslateService) {
+        this.loadData();
+
+        this.translateService.onLangChange.subscribe((event) => {
+            this.loadData();
+        });
+
     }
 
     async ngOnInit() {
+
     }
 
     async ionViewDidEnter() {
+    }
+
+
+    async loadData() {
+        this.subscriptions$.push(this.sentenceService.loadAvailableCategories().subscribe(async (categories) => {
+            this.availableCategories = categories
+            console.log(this.availableCategories);
+            this.countSentences();
+        }));
     }
 
 
@@ -30,14 +48,13 @@ export class ExplorePage implements OnInit, OnDestroy {
         this.subscriptions$.forEach((subscription) => subscription.unsubscribe());
     }
 
-    getRandomColor() {
-        const letters = '0123456789ABCDEF';
-        let color = '#';
-        // only accept darker colors
-        for (let i = 0; i < 3; i++) {
-            color += letters[Math.floor(Math.random() * 10)];
-        }
-        return color;
+    countSentences() {
+        this.availableCategories.forEach(category => {
+            this.subscriptions$.push(this.sentenceService.getSentencesByCategory(category).subscribe((sentences) => {
+                this.sentenceCountByCategory[category] = sentences.length;
+            }));
+        });
+
     }
 
     async openCategory(category: string) {
