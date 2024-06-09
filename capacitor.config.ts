70adc1c04d02db5ec5df1e390481a7ee2471b7a2
Exitare <13048528+Exitare@users.noreply.github.com>
2024-06-08 21:42:26 -0700

    Update app id

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,7 +1,7 @@
 import { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'com.inventirayastudios.storytime',
+  appId: 'com.inventirayastudios.paramathi',
   appName: 'Paramithi',
   webDir: 'www',
   server: {
