8c77ffc270682f4d0fe2fcb94be67fbd2af93b94
Exitare <business.exitare@icloud.com>
2024-10-02 14:02:15 -0700

    Add text to speech to story of the day

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -3,6 +3,8 @@ import {SentencesService} from "../../core/services/sentence.service";
 import {IAge, ISentence} from "../../core/models";
 import {SettingsService} from "../../core/services/settings.service";
 import {Subscription} from "rxjs";
+import {TextToSpeech} from '@capacitor-community/text-to-speech';
+import {TranslateService} from "@ngx-translate/core";
 
 @Component({
     selector: 'app-tab1',
@@ -15,8 +17,10 @@ export class Tab1Page implements OnInit, OnDestroy {
     userAge: number = 0;
     categories: string[] = [];
     userCategories: string[] = [];
+    language: string = 'en';
 
-    constructor(private sentenceService: SentencesService, private settingsService: SettingsService) {
+    constructor(private sentenceService: SentencesService, private settingsService: SettingsService,
+                private translateService: TranslateService) {
         this.subscriptions$.push(this.settingsService.selectedCategoriesChanged$.subscribe(async (selectedCategories: string[]) => {
             this.userCategories = selectedCategories;
             this.selectNewSentence();
@@ -26,6 +30,10 @@ export class Tab1Page implements OnInit, OnDestroy {
             this.userAge = age;
             this.selectNewSentence();
         }));
+
+        this.translateService.onLangChange.subscribe((langEvent) => {
+            this.language = langEvent.lang;
+        });
     }
 
     async ngOnInit() {
@@ -33,6 +41,8 @@ export class Tab1Page implements OnInit, OnDestroy {
         this.sentenceService.loadAvailableCategories().subscribe((categories) => {
             this.categories = categories;
         });
+
+        this.language = this.translateService.currentLang
     }
 
     ngOnDestroy() {
@@ -68,6 +78,38 @@ export class Tab1Page implements OnInit, OnDestroy {
                 console.log("Selected sentence", sentence);
             this.selectedSentence = sentence;
         });
+    }
+
+    async textToSpeech(event: any) {
+        event.stopPropagation();
+
+        if (!await TextToSpeech.isLanguageSupported({lang: this.language})) {
+            await TextToSpeech.speak({
+                text: "Sorry, the selected language is not supported by the text to speech engine.",
+                lang: 'en',
+                rate: 1.0,
+                pitch: 1.0,
+                volume: 1.0,
+                category: 'ambient',
+            });
+            return;
+        }
+
+        try {
+            // prevent other button clicks
+            await TextToSpeech.speak({
+                text: this.selectedSentence.sentence,
+                lang: this.language,
+                rate: 1.0,
+                pitch: 1.0,
+                volume: 1.0,
+                category: 'ambient',
+            });
+        } catch (e) {
+            console.error("Error while trying to speak", e);
+        }
+
+
     }
 
 }
