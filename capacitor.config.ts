601e78fc0399ab6fe4dd817abd04128a61650275
Exitare <business.exitare@icloud.com>
2024-10-05 19:54:21 -0700

    Squashed commit of the following:
    commit aca46fbf00656c11ad6ab89ae778e800eda824b4
    Author: Exitare <business.exitare@icloud.com>
    Date:   Sat Oct 5 19:53:54 2024 -0700
    
        Update package.json
    
    commit 82aa9a97e34d321dc4065757d21d631e5b7e070e
    Author: Exitare <business.exitare@icloud.com>
    Date:   Sat Oct 5 19:46:08 2024 -0700
    
        Upgrade angular to v18
    
    commit 46c75468c9bc2232dd4827c935cbbbdb1b08ccac
    Author: Exitare <business.exitare@icloud.com>
    Date:   Sat Oct 5 19:42:47 2024 -0700
    
        Update package.json
    
    commit 6bf2f32bd9ea972e6282f38b3cd33bb2c54be97e
    Author: Exitare <business.exitare@icloud.com>
    Date:   Sat Oct 5 19:39:02 2024 -0700
    
        Update capacitor/app

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -3,7 +3,7 @@ import { CapacitorConfig } from '@capacitor/cli';
 const config: CapacitorConfig = {
   appId: 'com.inventirayastudios.paramathi',
   appName: 'Paramithi',
-  webDir: 'www',
+  webDir: 'www/browser',
   server: {
     androidScheme: 'https'
   }
